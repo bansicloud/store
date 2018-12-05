f166e4038a87c29e69c377cc9252aad8b6c3ac4a
Daniil Okhlopkov <danokhlopkov@gmail.com>
2018-12-06 02:29:14 +0300

    refactor

--- add_file.sh
+++ add_file.sh
@@ -1,7 +1,7 @@
 # Adds a new file to repo
 
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+git config --global user.email "store@morejust.store"
+git config --global user.name "Mr. Store"
 
 # First argument - repo path
 BLOCKS_FOLDER_PATH=$1
@@ -11,54 +11,37 @@ BLOCKNAME="b1"
 FILEPATH=$2
 FILENAME=$(basename $FILEPATH)
 
-echo "BLOCK FOLDER PATH "$BLOCKS_FOLDER_PATH
-echo "FILEPATH "$FILEPATH
-echo "FILENAME "$FILENAME
+CURRENTDIR=$(pwd)
 
 # Maybe this should be replaced for non macOS systems:
 # md5 -> md5sum
-# HASHEDFILEPATH=$(echo -n $FILEPATH| md5sum | awk '{print $1}')
 
-# For debugging: TODO: revert to md5 hash
-BRANCHNAME=$FILENAME
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  BRANCHNAME=$(echo -n $FILEPATH| md5 | awk '{print $1}')
+else
+  BRANCHNAME=$(echo -n $FILEPATH| md5sum | awk '{print $1}')
+fi
 
-CURRENTDIR=$(pwd)
+git clone -b master --depth 1 https://ohld:$GITHUB_TOKEN@github.com/morejust/$BLOCKNAME.git $BLOCKS_FOLDER_PATH
 
 cd $BLOCKS_FOLDER_PATH
-
-# trying to clone repo
-
-# if [ ! -d "$BLOCKNAME" ]; then
-#   echo git clone https://ohld:$GITHUB_TOKEN@github.com/morejust/$BLOCKNAME.git
-#   git clone -b master --depth 1 https://ohld:$GITHUB_TOKEN@github.com/morejust/$BLOCKNAME.git
-# fi
-
-git clone -b master --depth 1 https://ohld:$GITHUB_TOKEN@github.com/morejust/$BLOCKNAME.git
-
 cd $BLOCKNAME
-
 # Checkout to the very first repo commit
-git checkout `git rev-list --max-parents=0 HEAD | tail -n 1`
+# git checkout `git rev-list --max-parents=0 HEAD | tail -n 1`
 
-git add .
-git reset --hard origin/master
+# git add .
+# git reset --hard origin/master
 git checkout -b $BRANCHNAME
 
 mv $FILEPATH ./
 
-# echo git status
-# git status
-
-
-echo git add $FILENAME
+# echo git add $FILENAME
 git add $FILENAME
 
-echo git commit -m "add $FILENAME to $BRANCHNAME branch"
+# echo git commit -m "add $FILENAME to $BRANCHNAME branch"
 git commit -m "add $FILENAME to $BRANCHNAME branch"
 
-# check if succeeded
-
-echo git push --set-upstream origin $BRANCHNAME
+# echo git push --set-upstream origin $BRANCHNAME
 git push --set-upstream origin $BRANCHNAME
 
 # Finishing
