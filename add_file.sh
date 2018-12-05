969edba44aa10ca146f074f16900c51dfce28ec0
Daniil Okhlopkov <danokhlopkov@gmail.com>
2018-12-06 02:05:18 +0300

    Update add_file.sh

--- add_file.sh
+++ add_file.sh
@@ -4,31 +4,38 @@ git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
 # First argument - repo path
-REPOPATH=$1
+BLOCKS_FOLDER_PATH=$1
+BLOCKNAME="b1"
 
 # Second argument - file-to-add path
 FILEPATH=$2
 FILENAME=$(basename $FILEPATH)
-REPONAME="b1"
+
+echo "BLOCK FOLDER PATH "$BLOCKS_FOLDER_PATH
+echo "FILEPATH "$FILEPATH
+echo "FILENAME "$FILENAME
 
 # Maybe this should be replaced for non macOS systems:
 # md5 -> md5sum
-HASHEDFILEPATH=$(echo -n $FILEPATH| md5sum | awk '{print $1}')
-BRANCHNAME=$HASHEDFILEPATH
+# HASHEDFILEPATH=$(echo -n $FILEPATH| md5sum | awk '{print $1}')
+
+# For debugging: TODO: revert to md5 hash
+BRANCHNAME=$FILENAME
 
 CURRENTDIR=$(pwd)
 
-cd $REPOPATH
+cd $BLOCKS_FOLDER_PATH
 
 # trying to clone repo
 
-# if [ no directory $REPONAME ]; do
-if [ ! -d "$REPONAME" ]; then
-  echo git clone https://ohld:$GITHUB_TOKEN@github.com/morejust/$REPONAME.git
-  git clone -b master --depth 1 https://ohld:$GITHUB_TOKEN@github.com/morejust/$REPONAME.git
-fi
+# if [ ! -d "$BLOCKNAME" ]; then
+#   echo git clone https://ohld:$GITHUB_TOKEN@github.com/morejust/$BLOCKNAME.git
+#   git clone -b master --depth 1 https://ohld:$GITHUB_TOKEN@github.com/morejust/$BLOCKNAME.git
+# fi
 
-cd $REPONAME
+git clone -b master --depth 1 https://ohld:$GITHUB_TOKEN@github.com/morejust/$BLOCKNAME.git
+
+cd $BLOCKNAME
 
 # Checkout to the very first repo commit
 git checkout `git rev-list --max-parents=0 HEAD | tail -n 1`
@@ -39,14 +46,11 @@ git checkout -b $BRANCHNAME
 
 mv $FILEPATH ./
 
-echo ls
-ls
+# echo git status
+# git status
 
-echo git status
-git status
-
-echo git push --set-upstream origin $BRANCHNAME
-git push --set-upstream origin $BRANCHNAME
+# echo git push --set-upstream origin $BRANCHNAME
+# git push --set-upstream origin $BRANCHNAME
 
 echo git add $FILENAME
 git add $FILENAME
@@ -55,8 +59,8 @@ echo git commit -m "add $FILENAME to $BRANCHNAME branch"
 git commit -m "add $FILENAME to $BRANCHNAME branch"
 
 # check if succeeded
-echo git commit -m "add $FILENAME to $BRANCHNAME branch"
-git commit -m "add $FILENAME to $BRANCHNAME branch"
+#echo git commit -m "add $FILENAME to $BRANCHNAME branch"
+#git commit -m "add $FILENAME to $BRANCHNAME branch"
 
 # Finishing
 # revert to commit
@@ -64,5 +68,5 @@ git checkout master
 git branch -D $BRANCHNAME
 cd $CURRENTDIR
 
-FILELINK="https://raw.githubusercontent.com/morejust/$REPONAME/$BRANCHNAME/$FILENAME"
+FILELINK="https://raw.githubusercontent.com/morejust/$BLOCKNAME/$BRANCHNAME/$FILENAME"
 echo $FILELINK
