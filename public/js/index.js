7b8c5336db44f9b5e90f66f4507be63533cd2c33
Your Name <you@example.com>
2018-12-06 02:48:24 +0300

    js: show links, loader on upload

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,36 @@
 const form = document.getElementById('form-upload');
 const input = document.getElementById('form-input');
+const loaded = document.querySelector('.loader');
+const file_list = document.querySelector('#uploaded-list');
+const file_link = document.querySelector('.uploaded-file-link');
+
 const DEV_API_ROOT = 'http://localhost:4000'
 const PROD_API_ROOT = 'https://morejust.herokuapp.com'
 const API_ROOT = window.location.hostname === 'localhost'
   ? DEV_API_ROOT
   : PROD_API_ROOT
+
+const showLoading = () => {
+  loaded.style.display = 'block'
+}
+
+const hideLoading = () => {
+  loaded.style.display = 'none'
+}
+
+const printFileList = (files) => {
+  const new_files_list = files.map(link => {
+    const node = file_link.cloneNode(true);
+
+    node.style.display = 'block';
+    node.querySelector('a').innerText = link;
+    node.querySelector('a').href = link;
+
+    return node
+  });
+
+  file_list.append(...new_files_list);
+}
 
 form.onsubmit = (e => {
   e.preventDefault();
@@ -14,6 +40,7 @@ form.onsubmit = (e => {
     return;
   }
   console.log('Generating links');
+  showLoading();
 
   var formData = new FormData(form);
 
@@ -26,5 +53,12 @@ form.onsubmit = (e => {
   })
   .then(data => {
     console.log(data);
+    printFileList(data);
+    hideLoading();
+  })
+  .catch(err => {
+    hideLoading();
+    console.error(err);
+    alert(err.name + ": " + err.message);
   });
 })
