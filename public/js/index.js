0cd8d3b664a7a39949928035ed1b5bff9f4698b1
Aleksey Bykhun <theisease@gmail.com>
2018-12-06 00:48:24 +0300

    client: if on gh-pages, use full heroku url

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,10 @@
 const form = document.getElementById('form-upload');
 const input = document.getElementById('form-input');
-const API_ROOT = 'http://localhost:4000'
+const DEV_API_ROOT = 'http://localhost:4000'
+const PROD_API_ROOT = 'https://morejust.herokuapp.com'
+const API_ROOT = window.location.host === 'morejust.store'
+  ? PROD_API_ROOT
+  : DEV_API_ROOT
 
 form.onsubmit = (e => {
   e.preventDefault();
