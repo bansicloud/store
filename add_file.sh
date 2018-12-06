c180d266b3740ac3e2f9f2671ad06fd1348dafbd
Mr. Store <store@morejust.store>
2018-12-07 00:30:36 +0300

    accept several files

--- add_file.sh
+++ add_file.sh
@@ -1,24 +1,19 @@
 # Adds a new file to repo
 
-# Argument - file-to-add path
-FILEPATH=$1
-FILENAME=$(basename $FILEPATH)
-
-
 # Consts
 BLOCKS_FOLDER_PATH="blocks/"
 BLOCKNAME="b1"
+CURRENTDIR=$(pwd)
 git config --global user.email "store@morejust.store"
 git config --global user.name "Mr. Store"
 
-CURRENTDIR=$(pwd)
-
 # solving problem with different func names (md5 / md5sum)
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  BRANCHNAME=$(echo -n $FILEPATH| md5 | awk '{print $1}')
+  BRANCHNAME=$(echo -n "$@"| md5 | awk '{print $1}')
 else
-  BRANCHNAME=$(echo -n $FILEPATH| md5sum | awk '{print $1}')
+  BRANCHNAME=$(echo -n "$@"| md5sum | awk '{print $1}')
 fi
+
 
 mkdir -p $BLOCKS_FOLDER_PATH
 cd $BLOCKS_FOLDER_PATH
@@ -28,15 +23,23 @@ git clone -b master --depth 1 https://ohld:$GITHUB_TOKEN@github.com/morejust/$BL
 cd $BLOCKNAME
 git checkout -b $BRANCHNAME
 
-mv $FILEPATH ./
-git add $FILENAME
-git commit -m "add $FILENAME to $BRANCHNAME branch"
+for i in "$@"
+do
+  FILEPATH=$i
+  FILENAME=$(basename $FILEPATH)
+	
+  mv $FILEPATH ./
+  git add $FILENAME
+
+  FILELINK="https://raw.githubusercontent.com/morejust/$BLOCKNAME/$BRANCHNAME/$FILENAME"
+  echo $FILELINK
+	
+done
+
+git commit -m "add $@ to $BRANCHNAME branch"
 git push --set-upstream origin $BRANCHNAME
 
 # Finishing
 git checkout master
 git branch -D $BRANCHNAME
 cd $CURRENTDIR
-
-FILELINK="https://raw.githubusercontent.com/morejust/$BLOCKNAME/$BRANCHNAME/$FILENAME"
-echo $FILELINK
