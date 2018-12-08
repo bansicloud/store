21c7cf32e68982d7431040664484e6260f80d8df
Daniil Okhlopkov <danokhlopkov@gmail.com>
2018-12-08 15:54:29 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ npm run build-front
 git add client/build
 git commit --allow-empty -m "build heroku"
 # git push --force --set-upstream origin heroku-deploy
-git push --force --quiet "https://$GITHUB_TOKEN@github.com/morejust/store" heroku-deploy > /dev/null 2>&1"
+git push --force --quiet "https://$GITHUB_TOKEN@github.com/morejust/store" heroku-deploy > /dev/null 2>&1
 git checkout master
