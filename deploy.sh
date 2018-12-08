3e47517e40ab284aef7059d4a6fc99ef50ae9631
Aleksey Bykhun <theisease@gmail.com>
2018-12-08 14:49:58 +0300

    heroku: tell to use upstream branch

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git rebase master
 npm run build-front
 git add client/build
 git commit --allow-empty -m "build heroku"
-git push --force
+git push --force --set-upstream origin heroku-deploy
 git checkout master
