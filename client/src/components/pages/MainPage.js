4bc5e93e3766f65f7ed0981d3a26890ea9a13385
Mr. Store <store@morejust.store>
2018-12-22 17:58:15 +0200

    prod api fix

--- client/src/components/pages/MainPage.js
+++ client/src/components/pages/MainPage.js
@@ -7,8 +7,7 @@ import RightBlock from '../MainPage/RightBlock';
 
 // Local Settings of your app
 const DEV_API_ROOT = 'http://localhost:4000';
-// const PROD_API_ROOT = 'https://morejust.herokuapp.com';
-const PROD_API_ROOT = 'https://morejust.store';
+const PROD_API_ROOT = 'https://morejust.herokuapp.com';
 
 class MainPage extends Component {
   constructor(props) {
