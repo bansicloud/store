3283372329190fd4b42c0144dd4789198c8b4796
Aleksey Bykhun <theisease@gmail.com>
2018-12-26 06:39:45 +0300

    deploy: update branch before push

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,26 @@ fi
 
 # update heroku-deploy branch
 rm -rf client/build
-git checkout -b heroku-deploy
-git fetch
-git pull origin heroku-deploy
+git branch heroku-deploy
+git checkout heroku-deploy
+
+if [ -z $CI ]
+then
+	echo local
+	git remote add upstream git@github.com:morejust/store.git
+else
+	echo ci
+	git remote add upstream "https://$GITHUB_TOKEN@github.com/morejust/store.git"
+fi
+
+git fetch upstream
+git reset --hard upstream/heroku-deploy
 
 # take all files from master
 git checkout master .
 git add .
-git reset client/.gitignore
+# except for the file which makes build dir ignored
+git checkout heroku-deploy client/.gitignore
 
 # build front and add to commit
 npm run build-front
@@ -27,14 +39,9 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 git commit --allow-empty -m "build heroku $DATE"
 
 # send updates to branch
-if [ -z $CI ]
-then
-	echo push local
-	git push --set-upstream origin heroku-deploy
-else
-	echo push ci
-	git push --quiet "https://$GITHUB_TOKEN@github.com/morejust/store.git" master:heroku-deploy
-fi
+echo push
+git push --set-upstream upstream heroku-deploy
+
 
 # restore local state
 git reset --hard
@@ -44,4 +51,3 @@ if $LOCAL_CHANGES
 then
 	git stash pop
 fi
-
