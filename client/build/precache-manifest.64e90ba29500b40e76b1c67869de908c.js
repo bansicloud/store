44d923d97624341af5644138996bda34c7544f28
Aleksey Bykhun <theisease@gmail.com>
2018-12-26 03:38:11 +0300

    build heroku

---
+++ client/build/precache-manifest.64e90ba29500b40e76b1c67869de908c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7fdac58bac6734a1d592efd7b9d2b68",
+    "url": "/static/media/logo.c7fdac58.svg"
+  },
+  {
+    "revision": "6505b75ff467bd82028575e222f8cefa",
+    "url": "/static/media/info_graphic.6505b75f.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e1dcbfdec1d9dd64772",
+    "url": "/static/js/main.2e1dcbfd.chunk.js"
+  },
+  {
+    "revision": "62c563de154f35e9d051",
+    "url": "/static/js/1.62c563de.chunk.js"
+  },
+  {
+    "revision": "62c563de154f35e9d051",
+    "url": "/static/css/1.d91acb1f.chunk.css"
+  },
+  {
+    "revision": "1a8e2b21ecae36b981af10e7d156aca1",
+    "url": "/index.html"
+  }
+];
