594858597a358741346ba8bba8586f2a63d68cb0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-31 16:24:05 +0000

    Deploy morejust/store to github.com/morejust/store.git:gh-pages

--- precache-manifest.4df9526cffebfa10658350029667ffd0.js
+++ precache-manifest.4df9526cffebfa10658350029667ffd0.js
@@ -12,15 +12,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "dcc1e0b59cd94ef4bfae",
-    "url": "/static/js/main.dcc1e0b5.chunk.js"
+    "revision": "9ebc3e335bbe3b5e173a",
+    "url": "/static/js/main.9ebc3e33.chunk.js"
   },
   {
     "revision": "9b060e9493c3cb7da11a",
     "url": "/static/js/1.9b060e94.chunk.js"
   },
   {
-    "revision": "dcc1e0b59cd94ef4bfae",
+    "revision": "9ebc3e335bbe3b5e173a",
     "url": "/static/css/main.b5f58112.chunk.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.6b5173c7.chunk.css"
   },
   {
-    "revision": "65afecc6aef6a62a3f86f09f121624f7",
+    "revision": "f9457ecbd59e3063a158aa40ea41951d",
     "url": "/index.html"
   }
 ];
