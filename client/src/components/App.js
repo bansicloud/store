62d5a767c82829854a4a18abdf0ee71c7b8c75c0
Viktor Kirillov <mr.kirillov@icloud.com>
2019-01-02 00:47:11 +0200

    Try to fix ReactRouter for gh-pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 
 import MainPage from './pages/MainPage';
 import DocsPage from './pages/DocsPage';
 import LinksPage from './pages/LinksPage';
 
 const App = () => {
+  const history = createHistory({
+    basename: process.env.PUBLIC_URL,
+  });
+
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router history={history} basename={process.env.PUBLIC_URL}>
       <div>
         <Route path={process.env.PUBLIC_URL + "/"} exact component={MainPage} />
         <Route path={process.env.PUBLIC_URL + "/docs"} component={DocsPage} />
         <Route path={process.env.PUBLIC_URL + "/links"} component={LinksPage} />
       </div>
-    </BrowserRouter>
+    </Router>
   )
 }
 
