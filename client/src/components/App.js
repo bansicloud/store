7bf01233731be75cb07df72d3d4db6ea5f8a6880
Viktor Kirillov <mr.kirillov@icloud.com>
2019-01-02 01:08:34 +0200

    Switched to HashRouter to correctly work with gh-pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import { HashRouter, Route } from 'react-router-dom';
+import createHistory from "history/createBrowserHistory";
 
 import MainPage from './pages/MainPage';
 import DocsPage from './pages/DocsPage';
 import LinksPage from './pages/LinksPage';
 
 const App = () => {
-  const history = createHistory({
-    basename: process.env.PUBLIC_URL,
-  });
-
   return (
-    <Router history={history} basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL} history={createHistory({ basename: process.env.PUBLIC_URL })}>
       <div>
         <Route path={process.env.PUBLIC_URL + "/"} exact component={MainPage} />
-        <Route path={process.env.PUBLIC_URL + "/docs"} component={DocsPage} />
-        <Route path={process.env.PUBLIC_URL + "/links"} component={LinksPage} />
+        <Route path={process.env.PUBLIC_URL + "/docs"} exact component={DocsPage} />
+        <Route path={process.env.PUBLIC_URL + "/links"} exact component={LinksPage} />
       </div>
-    </Router>
+    </HashRouter>
   )
 }
 
