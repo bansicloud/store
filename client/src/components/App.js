8e3aba338c22daeb23dce427910775f79c041f12
Viktor Kirillov <mr.kirillov@icloud.com>
2019-01-02 11:45:28 +0200

    MaxFileSIze set to 100 && open file selector on submit button

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
-import createHistory from "history/createBrowserHistory";
 
 import MainPage from './pages/MainPage';
 import DocsPage from './pages/DocsPage';
@@ -8,7 +7,7 @@ import LinksPage from './pages/LinksPage';
 
 const App = () => {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL} history={createHistory({ basename: process.env.PUBLIC_URL })}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <div>
         <Route path={process.env.PUBLIC_URL + "/"} exact component={MainPage} />
         <Route path={process.env.PUBLIC_URL + "/docs"} exact component={DocsPage} />
