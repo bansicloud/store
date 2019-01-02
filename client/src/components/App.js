33fed45a1c6ab6287259fd6d7e3f2059ac4420f4
Viktor Kirillov <mr.kirillov@icloud.com>
2019-01-02 11:57:37 +0200

    Moved Yandex Metrica to React side

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
 import DocsPage from './pages/DocsPage';
 import LinksPage from './pages/LinksPage';
 
-const App = () => {
-  return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <div>
-        <Route path={process.env.PUBLIC_URL + "/"} exact component={MainPage} />
-        <Route path={process.env.PUBLIC_URL + "/docs"} exact component={DocsPage} />
-        <Route path={process.env.PUBLIC_URL + "/links"} exact component={LinksPage} />
-      </div>
-    </HashRouter>
-  )
+class App extends Component {
+  componentDidMount() {
+    this.refs.ym.innerHTML=`
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+       m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+       ym(51443497, "init", {
+            id:51443497,
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+       });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/51443497" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+    `;
+  }
+
+  render() {
+    return (
+      <HashRouter basename={process.env.PUBLIC_URL}>
+        <div>
+          <div ref="ym"></div>
+          <Route path={process.env.PUBLIC_URL + "/"} exact component={MainPage} />
+          <Route path={process.env.PUBLIC_URL + "/docs"} exact component={DocsPage} />
+          <Route path={process.env.PUBLIC_URL + "/links"} exact component={LinksPage} />
+        </div>
+      </HashRouter>
+    )
+  }
 }
 
 export default App;
