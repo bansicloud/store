c77188ee0a62d687365eeaa67b713ead36ac3784
Viktor Kirillov <mr.kirillov@icloud.com>
2019-01-02 13:15:34 +0200

    Single Page Apps for GitHub Pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
 import DocsPage from './pages/DocsPage';
